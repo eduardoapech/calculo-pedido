1d29e5f5bcc53c2888fe51810a3e4fd66be8196a
eduardoapech <eduardo.augusto.pech97@gmail.com>
2025-06-03 14:09:53 -0300

    fix: corrigido erros ao executar o programa

--- src/main/java/com/edu/calculo_pedido/CalculoPedidoApplication.java
+++ src/main/java/com/edu/calculo_pedido/CalculoPedidoApplication.java
@@ -1,13 +1,29 @@
 package com.edu.calculo_pedido;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+import com.edu.entities.Order;
+import com.edu.services.OrderService;
 
 @SpringBootApplication
-public class CalculoPedidoApplication {
+@ComponentScan(basePackages = "com.edu")
+public class CalculoPedidoApplication implements CommandLineRunner {
+
+	@Autowired
+	private OrderService orderService;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CalculoPedidoApplication.class, args);
+	}
+
+	public void run(String... args) throws Exception {
+		Order order1 = new Order(1034, 150.00, 20.0);
+		double total1 = orderService.total(order1);
+		System.out.printf("Pedido código %d%nValor total: R$ %.2f%n%n", order1.getCode(), total1);
 	}
 
 }
