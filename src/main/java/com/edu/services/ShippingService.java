20e910b8ba82fcf4ab9615c5d4033824414f381f
eduardoapech <eduardo.augusto.pech97@gmail.com>
2025-06-03 11:15:28 -0300

    implementando a lógica no shippingService

--- src/main/java/com/edu/services/ShippingService.java
+++ src/main/java/com/edu/services/ShippingService.java
@@ -6,9 +6,17 @@ import com.edu.entities.Order;
 
 @Service
 public class ShippingService {
-	
+
 	public double shipping(Order order) {
-		return order.getBasic() - order.getDiscount();
+		double basic = order.getBasic();
+
+		if (basic < 100.0) {
+			return 20.0;
+		} else if (basic < 200.0) {
+			return 12.0;
+		} else {
+			return 0.0;
+		}
 	}
 
 }
