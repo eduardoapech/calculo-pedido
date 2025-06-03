4df594b227e642dc03d26fec38d66fd2026e02f0
eduardoapech <eduardo.augusto.pech97@gmail.com>
2025-06-03 14:37:32 -0300

    feat: adicionando exemplos e testando o programa

--- src/main/java/com/edu/calculo_pedido/CalculoPedidoApplication.java
+++ src/main/java/com/edu/calculo_pedido/CalculoPedidoApplication.java
@@ -24,6 +24,14 @@ public class CalculoPedidoApplication implements CommandLineRunner {
 		Order order1 = new Order(1034, 150.00, 20.0);
 		double total1 = orderService.total(order1);
 		System.out.printf("Pedido código %d%nValor total: R$ %.2f%n%n", order1.getCode(), total1);
+		
+		Order order2 = new Order(2282, 800.00, 10.0);
+		double total2 = orderService.total(order2);
+		System.out.printf("Pedido código %d%nValor total: R$ %.2f%n%n", order2.getCode(), total2);
+		
+		Order order3 = new Order(1309, 95.90, 0.0);
+		double total3 = orderService.total(order3);
+		System.out.printf("Pedido código %d%nValor total: R$ %.2f%n%n", order3.getCode(), total3);
 	}
 
 }
