2ae2e261eeb1c0d4c30b3928b02827ed68273071
eduardoapech <eduardo.augusto.pech97@gmail.com>
2025-06-03 13:47:32 -0300

    refactor: simplificando a lógica no orderService

--- src/main/java/com/edu/services/OrderService.java
+++ src/main/java/com/edu/services/OrderService.java
@@ -6,11 +6,18 @@ import com.edu.entities.Order;
 
 @Service
 public class OrderService {
-	
+
 	private ShippingService shippingService;
-	
+
+	public OrderService(ShippingService shippingService) {
+		this.shippingService = shippingService;
+	}
+
 	public double total(Order order) {
-		return order.getCode() ;
+		double discount = order.getBasic() * order.getDiscount() / 100.0;
+		double valueDiscount = order.getBasic() - discount;
+		double freight = shippingService.shipping(order);
+		return valueDiscount + freight;
 	}
 
 }
